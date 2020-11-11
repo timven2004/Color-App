08e2931c9e6eb963be554b8fe5e68a3c26206af5
root <timven2004@gmail.com>
2020-11-11 04:39:54 +0000

    Used JSS

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,14 +1,31 @@
 import React, {Component} from "react";
 import ColorBox from "./ColorBox"
-import "./MiniPalette.css"
+// import "./MiniPalette.css"
+import { withStyles } from '@material-ui/core/styles';
+
+const styles ={
+MiniPalette: {
+	width: "450px",
+	height: "250px",
+	display: "flex",
+	flexFlow: "row wrap"
+},
+
+tinyColors: {
+	width:"20%",
+	height:"25%",
+}
+}
+
 
 function MiniPalette(props){
-	
+	const { classes } = props;
+
 	const colorBoxes = props.palette.colors.map(color=>{
-			return (<div className="tiny-colors" style={{background: color.color}}></div>)
+			return (<div className={classes.tinyColors} style={{background: color.color}}></div>)
 		})
 	
-	return(<div className="MiniPalette">
+	return(<div className={classes.MiniPalette}>
 		{colorBoxes}
 		
 		</div>
@@ -16,4 +33,4 @@ function MiniPalette(props){
 }
 
 
-export default MiniPalette
+export default withStyles(styles)(MiniPalette);
